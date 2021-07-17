6bf88326ed570014d2ef28bf407509baf7408597
Alfonso Cartes <alfonso@cartes.dev>
2021-07-17 14:08:05 +0100

    feat(post preview component): add other fields to post preview component

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,10 +1,17 @@
+import React, { FC } from "react";
+import Image from "next/image";
 import { Grid, IconButton, Paper, Typography } from "@material-ui/core";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
-import React, { FC } from "react";
 import { Post } from "../API";
 
 interface Props {
   post: Post;
+}
+function formatDatePosted(date: string): string {
+  const now = new Date(Date.now());
+  const current = new Date(date);
+  const diff = now.getTime() - current.getTime();
+  return (diff / 1000 / 60 / 60).toFixed(0);
 }
 
 const PostPreview: FC<Props> = ({ post }) => {
@@ -15,17 +22,32 @@ const PostPreview: FC<Props> = ({ post }) => {
         direction="row"
         justify="flex-start"
         alignItems="flex-start"
+        wrap="nowrap"
         spacing={3}
-        style={{ width: "100%", padding: 12, marginTop: 24 }}
+        style={{
+          padding: 12,
+          marginTop: 24,
+        }}
       >
-        <Grid item spacing={1} style={{ maxWidth: 128 }}>
+        <Grid item style={{ maxWidth: 128 }}>
           <Grid container direction="column" alignItems="center">
             <Grid item>
               <IconButton color="inherit">
                 <ArrowUpward />
               </IconButton>
             </Grid>
-            <Grid item>votes</Grid>
+            <Grid item>
+              <Grid container direction="column" alignItems="center">
+                <Grid item>
+                  <Typography variant="h6">
+                    {(post.upvotes - post.downvotes).toString()}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2">votes</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
             <Grid item>
               <IconButton color="inherit">
                 <ArrowDownward />
@@ -38,12 +60,27 @@ const PostPreview: FC<Props> = ({ post }) => {
           <Grid container direction="column" alignItems="flex-start">
             <Grid item>
               <Typography variant="body1">
-                Posted by <b>{post.owner}</b> at <b>{post.createdAt}</b>
+                Posted by <b>{post.owner}</b> {formatDatePosted(post.createdAt)}{" "}
+                hours ago
               </Typography>
             </Grid>
             <Grid item>
               <Typography variant="h2">{post.title}</Typography>
             </Grid>
+            <Grid item style={{ maxHeight: 32, overflowY: "hidden" }}>
+              <Typography variant="body1">{post.contents}</Typography>
+            </Grid>
+            {!post.image && (
+              <Grid item>
+                <Image
+                  src={"https://source.unsplash.com/random/980x540"}
+                  alt={post.title + " image"}
+                  width={980}
+                  height={540}
+                  layout="intrinsic"
+                />
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Grid>
