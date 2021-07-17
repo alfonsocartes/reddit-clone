27a2d11e78894b49100a6289989fc630d19a0d41
Alfonso Cartes <alfonso@cartes.dev>
2021-07-17 10:14:00 +0100

    feat(fetch api): fetch API for posts (client side for better TTFB)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
+import { API } from "aws-amplify";
+
+import { listPosts } from "../graphql/queries";
 import { useUser } from "../context/AuthContext";
+import { ListPostsQuery, Post } from "../API";
 
 const Home: React.FC = () => {
   const { user } = useUser();
-  console.log(user);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPostsFromAPI = async (): Promise<Post[]> => {
+      const allPosts = (await API.graphql({ query: listPosts })) as {
+        data: ListPostsQuery;
+        errors: any;
+      };
+      if (allPosts.data) {
+        setPosts(allPosts.data.listPosts.items as Post[]);
+        return allPosts.data.listPosts.items as Post[];
+      } else {
+        throw new Error("Could not get posts");
+      }
+    };
+
+    fetchPostsFromAPI();
+  }, []);
+
+  console.log("User", user);
+  console.log("Posts:", posts);
+
   return <Typography variant="h1">Hello World</Typography>;
 };
 
